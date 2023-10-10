246707c548006b6ffe8e0ab7f9db4891e8b75d7d
kartGRAM <ahuaya1024@gmail.com>
2023-10-11 00:00:37 +0900

    polarsを追加

--- GPyTest.py
+++ GPyTest.py
@@ -1,3 +1,13 @@
 import polars as pl
 
-print("done")
+df = pl.read_csv(
+    "TireData/Hoosier-LC0-Rim7/CorneringTest1.dat",
+    skip_rows=1,
+    skip_rows_after_header=1,
+    separator="\t",
+)
+
+meanTemp = ((pl.col("TSTC") + pl.col("TSTI") + pl.col("TSTO")) / 3).alias("TSTM")
+df = df.with_columns(meanTemp)
+print(df.columns)
+print(df.describe())

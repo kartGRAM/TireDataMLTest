2d922144d12f0c6a3cda8b55d713fb880b4ee329
kartGRAM <ahuaya1024@gmail.com>
2023-10-12 22:30:52 +0900

    GPyの実装開始

--- GPyTest.py
+++ GPyTest.py
@@ -1,6 +1,7 @@
 import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
+import GPy
 
 df = pl.concat(
     [
@@ -23,22 +24,34 @@ df = pl.concat(
 meanTemp = ((pl.col("TSTC") + pl.col("TSTI") + pl.col("TSTO")) / 3).alias("TSTM")
 # 輪荷重は鉛直下向きが負なので、鉛直下向きを正にしてしまう
 invFZ = pl.col("FZ") * -1
-# タイヤ温度が平均して50℃以下のデータは
+# タイヤ温度が平均して50℃以下のデータ捨てる
 df = df.with_columns(meanTemp, invFZ).filter(pl.col("TSTM") > 50)
 
-print(df.columns)
+# showData(df)
+
+# SA IA FZ P FYを取得
+df = df.select([pl.col("SA"), pl.col("IA"), pl.col("FZ"), pl.col("P"), pl.col("FY")])
 print(df.describe())
 
-# データを間引く（表示高速化）
-dfds = df[::100]
-sns.set()
+kernel = GPy.kern.RBF(4)
 
-cols = df.columns
-fig, axes = plt.subplots(2, 2, figsize=(50, 50))
-axes = axes.ravel()
-sns.lineplot(x="ET", y="TSTM", data=dfds, ax=axes[0])
-sns.scatterplot(x="SA", y="FY", data=dfds, ax=axes[1])
-sns.scatterplot(x="SA", y="MZ", data=dfds, ax=axes[2])
-sns.scatterplot(x="FZ", y="FY", data=dfds, ax=axes[3])
 
-plt.show()
+def showData(df: pl.DataFrame):
+    # 概要を表示
+    print(df.columns)
+    print(df.describe())
+
+    # データを間引く（表示高速化）
+    dfds = df[::100]
+    sns.set()
+
+    fig, axes = plt.subplots(2, 3, figsize=(50, 50))
+    axes = axes.ravel()
+    sns.lineplot(x="ET", y="TSTM", data=dfds, ax=axes[0])
+    sns.scatterplot(x="SA", y="FY", data=dfds, ax=axes[1])
+    sns.scatterplot(x="SA", y="MZ", data=dfds, ax=axes[2])
+    sns.scatterplot(x="FZ", y="FY", data=dfds, ax=axes[3])
+    sns.scatterplot(x="P", y="FY", data=dfds, ax=axes[4])
+    sns.scatterplot(x="IA", y="FY", data=dfds, ax=axes[5])
+
+    plt.show()

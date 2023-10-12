2b367436b2f732a1e3dd5fbbc4e3591da975b5a0
kartGRAM <ahuaya1024@gmail.com>
2023-10-12 23:34:22 +0900

    GPyのテストプログラムを作成

--- GPyTest.py
+++ GPyTest.py
@@ -2,6 +2,29 @@ import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
 import GPy
+import numpy as np
+
+
+def showData(df: pl.DataFrame):
+    # 概要を表示
+    print(df.columns)
+    print(df.describe())
+
+    # データを間引く（表示高速化）
+    dfds = df[::100]
+    sns.set()
+
+    fig, axes = plt.subplots(2, 3, figsize=(50, 50))
+    axes = axes.ravel()
+    sns.lineplot(x="ET", y="TSTM", data=dfds, ax=axes[0])
+    sns.scatterplot(x="SA", y="FY", data=dfds, ax=axes[1])
+    sns.scatterplot(x="SA", y="MZ", data=dfds, ax=axes[2])
+    sns.scatterplot(x="FZ", y="FY", data=dfds, ax=axes[3])
+    sns.scatterplot(x="P", y="FY", data=dfds, ax=axes[4])
+    sns.scatterplot(x="IA", y="FY", data=dfds, ax=axes[5])
+
+    plt.show()
+
 
 df = pl.concat(
     [
@@ -30,28 +53,31 @@ df = df.with_columns(meanTemp, invFZ).filter(pl.col("TSTM") > 50)
 # showData(df)
 
 # SA IA FZ P FYを取得
-df = df.select([pl.col("SA"), pl.col("IA"), pl.col("FZ"), pl.col("P"), pl.col("FY")])
-print(df.describe())
+X = df.select("SA", "IA", "FZ", "P")
+Y = df.select("FY")
+print(X.describe())
+print(Y.describe())
+X = X.to_numpy()
+Y = Y.to_numpy()
+print(X.shape)
+print(Y.shape)
+# showData(df)
+
+# 格子点を作成
+points = np.stack(
+    np.meshgrid(
+        np.linspace(-15, 15, 10),  # SA
+        np.arange(5),  # IA
+        np.linspace(0, 1500, 10),  # FZ
+        np.linspace(40, 110, 6),  # P
+    ),
+    axis=-1,
+).reshape([-1, 4])
+print(points.shape)
+
+# exit()
 
 kernel = GPy.kern.RBF(4)
-
-
-def showData(df: pl.DataFrame):
-    # 概要を表示
-    print(df.columns)
-    print(df.describe())
-
-    # データを間引く（表示高速化）
-    dfds = df[::100]
-    sns.set()
-
-    fig, axes = plt.subplots(2, 3, figsize=(50, 50))
-    axes = axes.ravel()
-    sns.lineplot(x="ET", y="TSTM", data=dfds, ax=axes[0])
-    sns.scatterplot(x="SA", y="FY", data=dfds, ax=axes[1])
-    sns.scatterplot(x="SA", y="MZ", data=dfds, ax=axes[2])
-    sns.scatterplot(x="FZ", y="FY", data=dfds, ax=axes[3])
-    sns.scatterplot(x="P", y="FY", data=dfds, ax=axes[4])
-    sns.scatterplot(x="IA", y="FY", data=dfds, ax=axes[5])
-
-    plt.show()
+m_sparse = GPy.models.SparseGPRegression(X, Y, kernel, Z=points)
+m_sparse.optimize()
+m_sparse.plot()
